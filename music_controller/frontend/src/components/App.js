cbeb717ba035e2fe1a5e83eb5c0d740941e663f0
Robert J. Regalado <rjregalado1979@gmail.com>
2023-01-26 01:51:56 +0800

    add another components

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-
+import HomePage from "./HomePage";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,7 +8,11 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
   }
 }
 
